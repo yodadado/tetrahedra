97b4246d0f985705b75923628d52e2b4de8e8697
marconak <marconak@gmail.com>
2013-10-30 12:49:04 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-//Add commentar
+///Add commentar
     QApplication app(argc, argv);
     if (!QGLFormat::hasOpenGL()) {
         cerr << "This system has no OpenGL support" << endl;
